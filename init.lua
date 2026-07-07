3f69196fbef2bf1416fd3d0485ff22b4270c6982
trippymajoKali <eliaritz17@gmail.com>
2026-07-08 00:45:36 +0300

    New additions for my neovim config

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
@@ -15,6 +18,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    -- MS VS color scheme
     {
         "Mofiqul/vscode.nvim",
         priority = 1000,
@@ -31,9 +35,88 @@ require("lazy").setup({
             vim.cmd.colorscheme("vscode")
         end,
     },
+    --
+    -- Autocompletion
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+
+        opts = {
+            keymap = {
+                preset = "default",
+            },
+
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 300,
+                },
+            },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+
+            fuzzy = {
+                implementation = "prefer_rust_with_warning",
+            },
+        },
+
+        opts_extend = { "sources.default" },
+    },
+    --
+    -- Status line 
+    {
+        "nvim-mini/mini.statusline",
+        version = false,
+        config = function()
+            local statusline = require("mini.statusline")
+
+            statusline.setup({
+                use_icons = true,
+            })
+
+            statusline.section_location = function()
+                return "%2l:%-2v"
+            end
+        end,
+      },
+      --
+      -- Fuzzy finder over lists
+      -- needed: sudo apt install ripgrep fd-find make 
+      {
+          "nvim-telescope/telescope.nvim",
+          version = "*",
+          dependencies = {
+              "nvim-lua/plenary.nvim",
+              {
+                  "nvim-telescope/telescope-fzf-native.nvim",
+                  build = "make",
+              },
+          },
+          config = function()
+              local telescope = require("telescope")
+              local builtin = require("telescope.builtin")
+
+              telescope.setup({})
+
+              pcall(telescope.load_extension, "fzf")
+
+              vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+              vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+              vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+              vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+              vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document symbols" })
+              vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols, { desc = "Workspace symbols" })
+          end,
+    },
 })
 
-vim.g.mapleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = false
